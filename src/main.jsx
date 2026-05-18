aec2bfd4b52d3a9702c5fb2406fc60820bec7968
murali18-a <muraliananth21@gmail.com>
2026-05-18 18:06:59 +0530

    hosted

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Senior-Citizen/">
           <App />
           <Toaster
             position="top-right"
